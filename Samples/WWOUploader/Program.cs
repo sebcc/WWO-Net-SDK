456e2ce8263ab805c9588d9ae34ae3bb88957f3b
sebastien.caron <caron147@gmail.com>
2017-05-14 00:10:51 -0400

    Update the sample program

--- Samples/WWOUploader/Program.cs
+++ Samples/WWOUploader/Program.cs
@@ -10,7 +10,7 @@
     {
         private static void Main(string[] args)
         {
-            var wonderwareOnlineClient = new WonderwareOnlineClient("PROVIDE TOKEN HERE");
+            var wonderwareOnlineClient = new WonderwareOnlineClient("Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6IjA2MmQ3MWM1LWMyYjEtNGFiZS1hYjc1LWI0MWYzZTViMjk1NyJ9.eyJEYXRhU291cmNlSWQiOiIwNTM0MGY5Yy1iZjY2LTQ2NjMtOTExMi1iN2ZlOTczMjE1ZjkiLCJ0eXBlIjoic2VydmljZSIsInZlcnNpb24iOiIxLjAiLCJ0ZW5hbnRpZCI6IjFkNGExNTZkLTZjYjQtNGExZC1iZDkyLWU3NTM0MjgwMDcxNiIsInNpaWQiOiI2ZTliOGQ5NS0zNDRiLTQ1OTMtODM1OS02NmU2MzEyZmRkODQiLCJqdGkiOiI4Y2JhM2EzNS1lMGY0LTQxZWMtODYxYi1kODJlODY1ZWFhZGUiLCJpc3MiOiJwcm9vZm9maWRlbnRpdHlzZXJ2aWNlIn0.A-lf7GyfzxLo1vij0DizW0924ggP1uB7tY20W2DiZqhzMkEU8SFV9mQeqQ2Dl-UBfi_IlU_RZtsSFuDL5PMByEKHNe1EZgeK2S3GuF7o_Xa8LX6oNkvEgmzn8gIH3QrMdy1CthYEpaZRIhi5LnlsLFZfYVgk-W_36ISpp6MvVqVqI27GZjyPnA5c6aPdkpK7WF2HLJl5h-Cw5qtnihUZF00f_Yim8ux1QhJDg50uLzt6L_9YWCYdIPQmA7ZnPwBJZJ6ZOmqShH9tC2BRIitf9NbcZMpE7nHY6sZkfObVQilfWMfpcTGSG3DLqOMxOZt9pZxHds2YlVz46cx8ASLQjw");
             var stopWatch = Stopwatch.StartNew();
 
             // Create tag
@@ -18,14 +18,12 @@
             tag.TagName = "DefaultTag";
             tag.DataType = DataType.Double;
             tag.EngUnit = "ms";
+            tag.AddTagExtendedProperty("Prop1", "PropValue");
 
             try
             {
-                for (int i = 0; i < 100; i++)
-                {
-                    wonderwareOnlineClient.AddTag(tag);
-                    Console.WriteLine($"Successfully created tag - {tag.TagName}.");
-                }
+                wonderwareOnlineClient.AddTag(tag);
+                Console.WriteLine($"Successfully created tag - {tag.TagName}.");
             }
             catch (Exception ex)
             {
